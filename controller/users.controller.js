754519066f054669072b9205f20719fbec731ada
vaishnavisantosh <vaishnavijawanjal29@gmail.com>
2020-01-21 20:26:56 +0530

    made changes as per feedback

--- controller/users.controller.js
+++ controller/users.controller.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable import/no-named-as-default */
 /* eslint-disable import/named */
 /* eslint-disable max-len */
 /* eslint-disable no-underscore-dangle */
@@ -6,69 +8,61 @@ import dotenv from 'dotenv';
 import bcrypt from 'bcrypt';
 import User from '../model/User.model';
 import UserActivity from '../model/UserActivity.model';
-import { LoginValidation, RegistrationValidation } from '../lib/validator';
+import { loginValidation, registrationValidation } from '../lib/validator';
+import UserServices from '../lib/userServices';
 
 dotenv.config({ path: './.env' });
 
 exports.SignUp = async (req, res) => {
-  const { error } = RegistrationValidation(req.body);
-  if (error) {
-    return res.send(error.details[0].message);
-  }
-
-  const isUserPresent = User.findOne({ email: req.body.email });
-  if (isUserPresent) return res.send('email already exists!!');
-
-  const salt = bcrypt.genSaltSync(10);
-  const encryptedPass = bcrypt.hashSync(req.body.password, salt);
-  const user = new User({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    password: encryptedPass,
-  });
-
   try {
-    await user.save();
-    return res.status(200).send('registration Successful');
+    const { error } = registrationValidation(req.body);
+    const emailExists = await User.findOne({ email: req.body.email });
+    if (emailExists) return res.status(400).send('email already exists!!');
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const salt = bcrypt.genSaltSync(10);
+    const encryptedPass = bcrypt.hashSync(req.body.password, salt);
+
+    const user = new User({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: encryptedPass,
+    });
+
+    const savedUser = await user.save();
+    return res.status(200).send(savedUser);
   } catch (err) {
     return res.status(400).send(err);
   }
 };
 
 exports.SignIn = async (req, res) => {
-  let isAdmin;
-  const { error } = LoginValidation(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+  try {
+    let isAdmin;
+    const { error } = loginValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send('email not found');
 
-  const user = User.findOne({ email: req.body.email });
-
-  if (user) {
-    const validPass = bcrypt.compare(req.body.password, user.password);
+    const validPass = await bcrypt.compare(req.body.password, user.password);
     if (!validPass) return res.status(400).send('incorrect password!!');
 
-    if (user.firstName === 'admin') {
+    if (user && user.firstName === 'admin') {
       isAdmin = true;
     } else {
       isAdmin = false;
     }
-    const userActivity = new UserActivity({
-      userId: user._id,
-      ipAddress: req.ip,
-      uaString: req.headers['user-agent'],
 
-    });
-    const token = jwt.sign({ _id: user._id, isAdmin }, process.env.TOKEN_SECRET);
-    res.header('authentication-token', token).send('logged in!!');
+    const loggedUser = await UserServices.saveLoggedInUser(req, res, user);
 
-    await userActivity.save();
-    res.send('logged in!');
+    const token = jwt.sign({ _id: user._id }, process.env.TOKENSECRET);
+    res.header('authentication-token', token);
+    return res.send(`logged in!! ${loggedUser}  authentication-token: ${token}`);
+  } catch (err) {
+    return res.send(err);
   }
-
-
-  return res.send('invalid user');
 };
-
 
 exports.ShowAllUser = async (req, res) => {
   let users;
@@ -76,7 +70,9 @@ exports.ShowAllUser = async (req, res) => {
   const token = req.headers.authorization;
   if (!token) return res.status(401).send('Access denied');
 
-  const decodedToken = jwt.verify(req.headers.authorization, process.env.TOKEN_SECRET);
+  const decodedToken = jwt.verify(req.headers.authorization, process.env.TOKENSECRET);
+
+
   if (decodedToken.isAdmin) {
     users = User.find();
   } else {
@@ -90,44 +86,49 @@ exports.ShowAllUser = async (req, res) => {
 };
 
 exports.ShowParticularUser = async (req, res) => {
-  const token = req.headers.authorization;
-  if (!token) return res.status(401).send('Access denied');
+  try {
+    const token = req.headers.authorization;
+    if (!token) return res.status(401).send('Access denied');
 
-  const decodedToken = jwt.verify(req.headers.authorization, process.env.TOKEN_SECRET);
+    const decodedToken = jwt.verify(req.headers.authorization, process.env.TOKENSECRET);
 
-  const user = User.find({ _id: decodedToken._id });
-  if (user) {
+    const user = await User.find({ _id: decodedToken._id });
     return res.status(200).send(user);
+  } catch (err) {
+    return res.status(400).send('something went wrong');
   }
-  return res.status(400).send('something went wrong');
 };
 
 exports.Update = async (req, res) => {
-  const token = req.headers.authorization;
-  if (!token) return res.status(401).send('Access denied');
+  try {
+    const token = req.headers.authorization;
+    if (!token) return res.status(401).send('Access denied');
 
-  const decodedToken = jwt.verify(req.headers.authorization, process.env.TOKEN_SECRET);
+    const decodedToken = jwt.verify(req.headers.authorization, process.env.TOKENSECRET);
 
-  const updatedUser = await User.findOneAndUpdate({ _id: decodedToken._id }, { $set: { firstName: req.body.firstName } }, { new: true }, (err, updatedObeject) => {
-    if (err) {
-      return res.send('Something wrong when updating data!');
-    }
-    return res.send(updatedObeject);
-  });
-  return res.status(200).send(`updated user : ${updatedUser}`);
+    const updatedUser = await UserServices.updateUser(req, res, decodedToken);
+
+    return res.status(200).send(`updated user : ${updatedUser}`);
+  } catch (error) {
+    return res.status(400).send('something went wrong');
+  }
 };
 
 exports.UserActivity = async (req, res) => {
-  const date = new Date();
-  const dt = date.setDate(date.getDate() - process.env.INACTIVEDAYS);
+  try {
+    const date = new Date();
+    const dt = date.setDate(date.getDate() - process.env.INACTIVEDAYS);
 
-  await UserActivity
+    await UserActivity
 
-    .find({ loginDate: { $lt: dt } })
+      .find({ loginDate: { $lt: dt } })
 
-    .populate('users')
+      .populate('users')
 
-    .exec();
+      .exec();
 
-  return res.status(200);
+    return res.status(200);
+  } catch (error) {
+    return res.status(400).send(error);
+  }
 };
